5e8c3d1c24da427e7bbcf8f261c32a6173e291a5
github4prabu <129778324+github4prabu@users.noreply.github.com>
2023-05-04 17:35:46 +0530

    Skeleton Project For Pharmacy Management System

---
+++ src/main/java/dedalus/phamansys/PhamansysApplication.java
@@ -0,0 +1,13 @@
+package dedalus.phamansys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamansysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamansysApplication.class, args);
+	}
+
+}
