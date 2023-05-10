3401e9de3d851a33ecd1d266884d51c6b545306c
Santhiya30 <73530516+Santhiya30@users.noreply.github.com>
2023-05-10 15:42:29 +0530

    Add files via upload

--- src/main/java/dedalus/phamansys/service/DrugServiceImpl.java
+++ src/main/java/dedalus/phamansys/service/DrugServiceImpl.java
@@ -3,13 +3,18 @@ package dedalus.phamansys.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-
+import dedalus.phamansys.entity.Drug;
 import dedalus.phamansys.repo.DrugRepository;
 
 @Service
-public class DrugServiceImpl {
+public class DrugServiceImpl implements DrugService {
 
 	@Autowired
 	private DrugRepository DrugRepository;
+
+	@Override
+	public Drug DrugDetail(Drug Drug) {
+		// TODO Auto-generated method stub
+		return DrugRepository.save(Drug);
+	}
 }
